6d83402662b7e5732e50243d71ed91e88b9d5e5d
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-06-27 16:16:51 -0400

    Pushing the playerController changes + new layer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    Score score;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float mouseRayDistance;
+
+    public RaycastHit mouseHit;
+    public Ray mouseRay;
+    public Vector3 mousePosition;
+    public GameObject hit;
+
+    private LayerMask interactiveMask;
+
+
+    private void Awake()
+    {
+        score = GameObject.FindGameObjectWithTag("BallTag").GetComponent<Score>();
+        interactiveMask = LayerMask.GetMask("BallLayer");
+    }
+
+
+    private void Update()
+    {
+        DebugRaycast();
+        FoundBallCheck();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (FoundBallCheck() == true)
+            {
+                if (hitObject().transform.tag == "BallTag")
+                {
+                    score.ScoreKeeper();
+                }
+            }
+            else { print("Debug: Found Nothing"); }
+        }
+    }
+
+    void DebugRaycast()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(mousePosition, mouseRay.direction * mouseRayDistance, Color.green);
+    }
+
+    public bool FoundBallCheck()
+    {
+        return Physics.Raycast(mouseRay, out mouseHit, mouseRayDistance, interactiveMask);
+    }
+
+    public GameObject hitObject()
+    {
+        hit = mouseHit.transform.gameObject;
+        return hit;
+    }
 }
